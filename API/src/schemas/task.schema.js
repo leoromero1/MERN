161e4a5f8ef079ae575d65685fa72afea60ea07d
Leo Romero <leoromero.bls@gmail.com>
2023-06-28 14:59:49 -0300

    add schema

--- API/src/schemas/task.schema.js
+++ API/src/schemas/task.schema.js
@@ -3,13 +3,13 @@ import { z } from "zod";
 export const taskSchema = z.object({
   title: z.string({
     required_error: 'please enter a title'
-  }).min(4, {
-    message: "Title must be at least 4 characters",
+  }).min(2, {
+    message: "Title must be at least 2 characters",
   }),
   description: z.string({
     required_error: 'please enter a description'
-  }).min(10, {
-    message: "Title must be at least 10 characters",
+  }).min(3, {
+    message: "Description must be at least 3 characters",
   }),
   date: z.string().datetime().optional()
 })
